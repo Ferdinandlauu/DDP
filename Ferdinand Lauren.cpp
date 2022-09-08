c911dadbed3101c4b8f860f1e300d7db3e3e6dea
Ferdinandlauu <112993159+Ferdinandlauu@users.noreply.github.com>
2022-09-08 19:57:52 +0700

    Add files via upload

---
+++ Ferdinand Lauren.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int x, y;
+	cout << "Nilai x : ";
+	cin >> x;
+	
+	cout << "Nilai y : ";
+	cin >> y;
+
+	cout << "Hasil x + y: " << x + y << endl;
+    cout << "Hasil x - y: " << x - y << endl;
+    cout << "Hasil x * y: " << x * y << endl;
+    cout << "Hasil x / y: " << x / y << endl;
+    cout << "Hasil x % y: " << x % y << endl;
+    
+    return 0;
+}
